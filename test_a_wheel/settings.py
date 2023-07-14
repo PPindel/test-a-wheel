dbe56aaff08dda06dae25d37f405c71d2f19f86e
PPindel <przemyslaw.pindel@gmail.com>
2023-07-14 09:10:14 +0000

    Rename some variables and functions to keep consistency

--- test_a_wheel/settings.py
+++ test_a_wheel/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'cart.contexts.bag_contents',
+                'cart.contexts.cart_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
