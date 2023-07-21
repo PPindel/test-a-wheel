1da55d67fc6eb821f68535e1bc2e8485b7379000
PPindel <przemyslaw.pindel@gmail.com>
2023-07-21 10:31:12 +0000

    Updated products html according to a new product model

--- test_a_wheel/settings.py
+++ test_a_wheel/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ppindel-testawheel-fmsy5ct7l7j.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = ['8000-ppindel-testawheel-fmsy5ct7l7j.ws-eu102.gitpod.io']
 
 
 # Application definition
