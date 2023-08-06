e866df33e996e96a2cbb258c3e9cc6f519a1a116
PPindel <przemyslaw.pindel@gmail.com>
2023-08-06 14:44:28 +0000

    Fix accidentially removed messages JS block

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404  # noqa E501
 from django.contrib import messages
 
 from products.models import Product
@@ -21,7 +21,7 @@ def add_to_cart(request, item_id):
     cart = request.session.get('cart', {})
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
-        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')  # noqa E501
     else:
         cart[item_id] = quantity
         messages.success(request, f'Added {product.name} to your cart')
@@ -39,7 +39,7 @@ def adjust_cart(request, item_id):
 
     if quantity > 0:
         cart[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')  # noqa E501
     else:
         cart.pop(item_id)
         messages.success(request, f'Removed {product.name} from your cart')
