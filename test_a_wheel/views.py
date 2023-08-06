539b8642e5881809a9a42d596d5a3597bf799fc5
PPindel <przemyslaw.pindel@gmail.com>
2023-08-06 14:19:15 +0000

    Add 403 and 500 errors custom pages

--- test_a_wheel/views.py
+++ test_a_wheel/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 
+def handler403(request, exception):
+    """ Error Handler 403 - Page Not Allowed """
+    return render(request, "errors/403.html", status=403)
+
+
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "errors/404.html", status=404)
+
+
+def handler500(request, exception):
+    """ Error Handler 404 - Internal Server Error """
+    return render(request, "errors/500.html", status=500)
