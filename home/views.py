a2a632428bc0a5d12dc24984f68ba983500da83e
PPindel <przemyslaw.pindel@gmail.com>
2023-08-08 19:52:59 +0000

    Add comments to home app

--- home/views.py
+++ home/views.py
@@ -1,21 +1,20 @@
+# Import necessary module for rendering views
 from django.shortcuts import render
 
-# Create your views here.
 
-
+# View to render the index page
 def index(request):
     """ A view to render index page """
-
     return render(request, 'home/index.html')
 
 
+# View to render the contact page
 def contact(request):
     """ A view to render contact page """
-
     return render(request, 'home/contact.html')
 
 
+# View to render the FAQ page
 def faq(request):
-    """ A view to render contact page """
-
+    """ A view to render faq page """
     return render(request, 'home/faq.html')
